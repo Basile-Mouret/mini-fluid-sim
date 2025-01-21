79bd3e45e389d12aabcb614678b88b69378444c6
Basile Mouret <basile.mouret234@gmail.com>
2025-01-21 17:50:11 +0000

    end fedora

--- math.f90
+++ math.f90
@@ -2,42 +2,70 @@ program arrays
 
     implicit none
 
-    
-
-    type :: cell
-        double precision  :: density
-        double precision , dimension(2) :: velocity
-    end type cell
-
-    type(cell), dimension(5,5) :: cells
+    integer, parameter :: dim = 5
+    real, dimension(dim+1,dim+1) :: u
+    real, dimension(dim+1,dim+1) :: v
+    real, dimension(dim,dim) :: divergence
+    ! real, dimension(dim,dim) :: pressure
+    real, dimension(0:dim+1,0:dim+1) :: is_fluid
     double precision :: dt
     integer :: nsteps
     integer :: i, j, n
-
+    integer, parameter :: g = 1
+    real :: inv_nb_border_fluid_cells
 
    ! Initialize random seed
     call random_seed()
 
     ! Initialize the cells array
-    do i=1, 5
-        do j=1, 5
-            call random_number(cells(i,j)%velocity)
-            ! cells(i,j)%velocity = [0.0, 0.0]
-            cells(i,j)%density = 1.0
+    do i=1, dim+1
+        do j=1, dim+1
+            if (i == 0 .OR. i==dim+1) then
+                u(i,j) = 0
+            else 
+                v(i,j) = 1
+            end if
+            if (j==0 .OR. j==dim+1) then
+                v(i,j) = 0
+            else 
+                v(i,j) = 1
+            end if
         end do
     end do
+
+    do i=1, dim
+        do j=1, dim
+            divergence(i,j) = u(i,j) - u(i+1,j) + v(i,j) - v(i,j+1)
+        end do
+    end do
+
+    do i=0, dim+1
+        do j=0, dim+1
+            if (i==0 .OR. i==dim+1 .OR. j==0 .OR. j==dim+1) then
+                is_fluid(i,j) = 0
+            else
+                is_fluid(i,j) = 1 ! 0 if it's a border cell, 1 otherwise
+            end if
+        end do
+    end do
+
+
+
     dt = 1.0/60.0
     print *,dt
-    nsteps = 1!00
+    nsteps = 100
 
 
     do n=1, nsteps
-        ! Update the cells
-        do i=1, 5
-            do j=1, 5
-                ! calculate the new density & velocity
-                cells(i,j)%velocity(1) = cells(i,j)%velocity(1) + dt
-                print '("tour",I3, I2, I2, ": (", F6.3, ",", F6.3, ")")', n, i, j, cells(i, j)%velocity(1), cells(i, j)%velocity(2)
+        ! Update the cells 
+        do i=1, dim
+            do j=1, dim
+                inv_nb_border_fluid_cells = 1/(is_fluid(i+1,j)+is_fluid(i-1,j)+is_fluid(i,j+1)+is_fluid(i,j-1))
+                ! calculate the new velocity so that there is no divergence
+                u(i,j) = u(i,j) + divergence(i,j)*is_fluid(i-1,j)*inv_nb_border_fluid_cells
+                v(i,j) = v(i,j) + divergence(i,j)*is_fluid(i+1,j)*inv_nb_border_fluid_cells
+                u(i+1,j) = u(i+1,j) - divergence(i,j)*is_fluid(i,j-1)*inv_nb_border_fluid_cells
+                v(i,j+1) = v(i,j+1) - divergence(i,j)*is_fluid(i,j+1)*inv_nb_border_fluid_cells
             end do
         end do
     end do
